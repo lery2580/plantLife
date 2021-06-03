8d7c663fa8ef0f110b28a2c513d4d78e1156542e
lery2580 <73690399+lery2580@users.noreply.github.com>
2021-06-03 22:27:59 +0800

    first commit

--- slides.js
+++ slides.js
@@ -7,9 +7,30 @@ let z = 1
 slideArea.addEventListener("click", function(){
 
     currentSlide = currentSlide + 1
+    if(currentSlide > images.length -1)
+    {
+        currentSlide = 0
+    }
+
+
+
     z = z + 1
 
+    images.forEach(function(image){
+        image.style.animation = ""
+    })
+
     images[currentSlide].style.zIndex = z
+    images[currentSlide].style.animation = "fade 0.5s"
 })
 
+
+slideArea.addEventListener("mouseover", function(){
+    images.forEach(image => {
+        const x = 100 * Math.random() -50
+        const y = 100 * Math.random() -50
+
+        image.style.transform =  `translate(${x}px, ${y}px)`
+    })
+})
 
